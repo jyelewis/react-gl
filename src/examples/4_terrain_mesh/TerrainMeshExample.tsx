1ec3c6e02b7d7056a8b5fb4622265cd12877205b
Jye Lewis <jye@jyelewis.com>
2022-07-12 18:40:13 +1000

    Publish to gh-pages

--- src/examples/4_terrain_mesh/TerrainMeshExample.tsx
+++ src/examples/4_terrain_mesh/TerrainMeshExample.tsx
@@ -7,7 +7,7 @@ const tileX = 15066;
 const tileY = 9841;
 
 const mapboxKey =
-  "pk.eyJ1IjoianllbGV3aXMtcGVyc29uYWxwcm9qZWN0czIiLCJhIjoiY2t5NHNhcnUxMGRhdzJvbzNtaTBnYzRldCJ9.6gD89bYc9IHL-hQIIi-D3g";
+  "pk.eyJ1IjoianllbGV3aXMtcGVyc29uYWxwcm9qZWN0czIiLCJhIjoiY2t5NHM2OXA4MGVqbDJ1bzRrMmduMTh6NSJ9.WP0xw2m5HI9LriwsZ1-VGQ";
 const terrainImageUrl = `https://api.mapbox.com/v4/mapbox.terrain-rgb/${tileZoom}/${tileX}/${tileY}.png?access_token=${mapboxKey}`;
 const orthoImageUrl = `https://api.mapbox.com/v4/mapbox.satellite/${tileZoom}/${tileX}/${tileY}.png?access_token=${mapboxKey}`;
 
